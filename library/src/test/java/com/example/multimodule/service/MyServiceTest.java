eeb8ee6518b8a8cd16b11b867ee7c984b66e1e8f
loic-roux-404 <loic.roux.404@gmail.com>
2022-07-15 16:46:27 +0200

    generated apis

--- library/src/test/java/com/example/multimodule/service/MyServiceTest.java
+++ library/src/test/java/com/example/multimodule/service/MyServiceTest.java
@@ -1,26 +1,8 @@
 package com.example.multimodule.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest("service.message=Hello")
+@SpringBootTest
 public class MyServiceTest {
-
-	@Autowired
-	private MyService myService;
-
-	@Test
-	public void contextLoads() {
-		assertThat(myService.message()).isNotNull();
-	}
-
-	@SpringBootApplication
-	static class TestConfiguration {
-	}
 
 }
